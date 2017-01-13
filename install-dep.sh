66e8ba8a9ee1265dabe6e285879b20bfd126a13e
kairen.bai <s1499k005@gmail.com>
2017-01-13 10:22:37 +0800

    Add addons site and modify some roles

--- install-dep.sh
+++ install-dep.sh
@@ -2,10 +2,22 @@
 
 set -xe
 
-sudo yum install -y gcc git vim openssl-devel \
+sudo yum install -y gcc git vim openssl-devel expect \
                     python python-devel python-setuptools
 
 sudo easy_install pip
 sudo pip install  ansible
+
+sudo su -
+
+expect -c "
+spawn ssh-keygen -t rsa  \"\"
+expect \"Enter passphrase (empty for no passphrase):\"
+send \"\r\"
+expect \"Enter same passphrase again:\"
+send \"\r\"
+"
+
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 
 git clone https://github.com/kairen/kubernetes-ceph-ansible.git -b dev
