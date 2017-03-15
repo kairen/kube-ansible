5a41872c6b2fd066e7c8fb471b33389ee7fb044d
kairen.bai <s1499k005@gmail.com>
2017-03-15 11:42:09 +0800

    Add setup-vagrant options

--- config.rb
+++ config.rb
@@ -1,23 +1,23 @@
 # Machine configuration variable
 # examples : "bento/centos-7.2", "bento/ubuntu-16.04", "coreos-{alpha/beta/stable}"
-$box_image       = "bento/ubuntu-16.04"
-$coreos_version  = "current"
+$box_image = "bento/ubuntu-16.04"
+$coreos_version = "current"
 
-$master_count    = 1 # The HA-cluster at least 3 node.
-$node_count      = 2
-$disk_count      = 0
-$storage_path    = "./tmp/"
-$storage_size    = "20480"
+$master_count = 1 # The HA-cluster at least 3 node.
+$node_count = 2
+$disk_count = 0
+$storage_path = "./tmp/"
+$storage_size = "20480"
 
-$system_memory   = 1024 # If enable 'ceph-cluster', the memory need more than 2048MB.
-$system_vcpus    = 1
+$system_memory = 1024 # If enable 'ceph-cluster', the memory need more than 2048MB.
+$system_vcpus = 1
 
-$bridge_enable   = false
-$bridge_eth      = "eno1"
-$private_subnet  = "172.16.35"
-$private_count   = 10
+$bridge_enable = false
+$bridge_eth = "eno1"
+$private_subnet = "172.16.35"
+$private_count = 10
 
 # Name prefix
-$prefix_name   = "kube"
+$prefix_name = "kube"
 $master_prefix = "master"
-$node_prefix   = "node"
+$node_prefix = "node"
