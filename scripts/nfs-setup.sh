05064bc49f827b2164b71188b58cb275014ecda2
kairen.bai <s1499k005@gmail.com>
2017-04-06 10:58:21 +0800

    Fix nfs-setup script

--- scripts/nfs-setup.sh
+++ scripts/nfs-setup.sh
@@ -11,7 +11,7 @@ sudo apt-get -y install nfs-kernel-server
 sudo mkdir -p /var/nfs/data
 
 cat <<EOF > /etc/exports
-/var/nfs/data 172.16.35.0/24(rw,sync,no_root_squash,no_subtree_check)
+/var/nfs/data *(rw,sync,no_root_squash,no_subtree_check)
 EOF
 
 sudo /etc/init.d/nfs-kernel-server restart
