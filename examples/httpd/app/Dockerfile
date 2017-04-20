d8bbdc7c461aa9b0993726ca9b2510723603197c
kairen.bai <s1499k005@gmail.com>
2017-04-20 09:55:31 +0800

    Remove httpd example

--- examples/httpd/app/Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM httpd:2.4
-COPY ./html/ /usr/local/apache2/htdocs/
--- examples/httpd/app/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z ${1} ]; then
-   echo "Input version number."
-   exit 1
-fi
-
-docker build -t "kyle/web-app:${1}" .
